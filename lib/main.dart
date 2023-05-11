72a438973c6eeae754b23931272b764cad7df2d8
ybouali <yassine.bouali.bo@gmail.com>
2023-05-11 18:56:22 +0100

    STARTING MY FIRST APP FLUTTER

---
+++ lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Coffee_Masters',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Coffee Masters'),
+    );
+  }
+}
+
+
+class HelloWidget extends StatefulWidget {
+
+  const HelloWidget({super.key});
+
+  @override
+  State<HelloWidget> createState() => _HelloWidgetState();
+}
+
+class _HelloWidgetState extends State<HelloWidget> {
+  
+  String name = 'Yassine';
+  
+  @override
+  Widget build(BuildContext context) {
+
+    return Column(
+      children: [
+        Row(
+          children: [
+            Text('Hello, $name !\n'),
+          ],
+        ),
+        TextField(
+          onChanged: (value) => setState(() {
+            name = value;
+          }),
+        ),
+      ],
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: const HelloWidget(),
+    );
+  }
+}
